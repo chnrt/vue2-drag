7c7060715b303115aa0f873448c5e45ff1e32fcd
IMustTo <cxjbinfo@gmail.com>
2016-10-12 14:33:44 +0800

    add element

--- drag/src/App.vue
+++ drag/src/App.vue
@@ -1,15 +1,31 @@
 <template>
+<div>
+  <el-menu theme="dark" default-active="1" class="el-menu-demo" mode="horizontal">
+    <el-menu-item index="1">处理中心</el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">我的工作台</template>
+      <el-menu-item index="2-1">选项1</el-menu-item>
+      <el-menu-item index="2-2">选项2</el-menu-item>
+      <el-menu-item index="2-3">选项3</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="3">订单管理</el-menu-item>
+  </el-menu>
+  <div class="line"></div>
 
+  <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="text">文字按钮</el-button>
+</div>
 </template>
 
 <script>
-
 export default {
   components: {
+    // Button,
   },
 };
 </script>
 
 <style>
-
+@import url('./assets/css/reset.css');
 </style>
--- drag/src/main.js
+++ drag/src/main.js
@@ -1,7 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Element from 'element-ui';
+
 import App from './App';
+
+Vue.use(Element);
+// Vue.component(Select.name, Select);
+// Vue.component(Button.name, Button);
+// Vue.component(Nav.name, Nav);
 
 /* eslint-disable no-new */
 new Vue({
